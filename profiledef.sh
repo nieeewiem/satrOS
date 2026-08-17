e1fe8b2fde1d89ac108201e851fe5c81297ada3d
Zakochanyyy <adameczekitsme@gmail.com>
2026-08-17 07:37:21 +0000

    Add OrionOS branding: profiledef.sh and os-release

--- profiledef.sh
+++ profiledef.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_name="orionos"
+iso_label="ORION_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="OrionOS <https://orionos.org>"
+iso_application="OrionOS Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
-install_dir="arch"
+install_dir="orionos"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
            'uefi.systemd-boot')
